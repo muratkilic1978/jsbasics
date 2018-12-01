afdd48c2bddc6053784068d588e92cd579467270
muratkilic1978 <muratkilic1978@gmail.com>
2018-12-01 12:06:58 +0100

    js variables and datatypes introduced

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-console.log('Murat');
-alert("Murat Kilic");
-document.write('Murat Kilic');
+//String variable
+var firstName = 'Murat Kilic';
+console.log(firstName);
+
+//Number variable. Hint is decimalnumbers
+var age = 41;
+
+//Boolean variable
+var isPensioner = false;
+console.log(isPensioner);
+
+
+//Undefined variable
+var job = "Senior lecturer"
+
+console.log(job);
+
+//Error syntax in JS console
+
+//var 40years = 40;
+//var year&old = 40;
+//var function = 40;
+//var if = 40;
+
+/*
+Always use english characters in JS programming language
+
+Don't use spaces in between variables
+Don't begin a variable with number or symbols
+To increase readability you should always use camel notation
+
+*/
