d930737eda466ba9826d991c6db255e1e18b19d7
muratkilic1978 <muratkilic1978@gmail.com>
2018-12-06 07:30:42 +0100

    js truthy and falsy values and equality operators

--- js/script.js
+++ js/script.js
@@ -240,6 +240,7 @@ if (age < 13 ) {
 */
 
 // Ternary operator
+/*
 var firstName, age, drink;
 firstName = 'John';
 age = 40;
@@ -297,4 +298,26 @@ switch(true) {
     default: 
         console.log(firstName + ' is a pensioner.'); 
     } 
-  
+*/
+
+/***************************
+*  Truthy and falsy values and equality operators
+*  
+*/
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 23;
+if (height || height === 0) {
+    console.log('Variable is defined!');
+} else {
+    console.log('Variable has NOT been defined!');
+}
+
+// Equality operators
+if (height === '23') {
+    console.log('The == operator does type coercion!');
+}
