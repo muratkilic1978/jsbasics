a6f4f363cda293e7f019b7c23b0275875db4bc53
muratkilic1978 <muratkilic1978@gmail.com>
2019-01-04 12:42:18 +0100

    js challenger 02 - calculate average score and decide which team has the highest average score

--- js/script.js
+++ js/script.js
@@ -308,6 +308,7 @@ switch(true) {
 // falsy values: undefined, null, 0, '', NaN
 // truthy values: NOT falsy values
 
+/*
 var height;
 
 height = 23;
@@ -321,3 +322,17 @@ if (height || height === 0) {
 if (height === '23') {
     console.log('The == operator does type coercion!');
 }
+*/
+
+/***************************
+*  JS Challenge 01
+*  
+*/
+/* John and Mike both play basketball in different teams. In the latest 3 games, John team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Donøt forget to take into account there might be a draw (the same average score).
+
+GOOD LUCK :-)
+*/
