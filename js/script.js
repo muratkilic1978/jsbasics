9c3908f69aa1aa341a1863beb42baea5226663a0
muratkilic1978 <muratkilic1978@gmail.com>
2019-01-05 10:19:08 +0100

    js challenge 03

--- js/script.js
+++ js/script.js
@@ -432,6 +432,8 @@ console.log(whatDoYouWorkWith('laywer', 'Melissa'));
 *  JS Arrays
 *  
 */
+
+/*
 // Initialize new array
 var names = ['John', 'Melissa', 'Mark', 'Luis', 'Jenniffer'];
 var years = new Array(1864, 1970, 1999, 2001, 2008);
@@ -472,4 +474,20 @@ console.log(john.indexOf('George')); //returns -1 if the element is not included
 var isDesignerIncluded = john.indexOf('teacher') === -1 ? 'designer IS NOT included in the array' : 'designer is included in the array';
 
 console.log(isDesignerIncluded);
+*/
 
+/***************************
+*  JS Challenge 03
+*  
+*/
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1. Containing all three tips (one for each bill)
+2. Containing all three final paid amounts (bill + tip)
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK :-)
